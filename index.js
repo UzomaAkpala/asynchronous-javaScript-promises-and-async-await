2edfef916072c09319442946ba3e2610e3ef7368
UzomaAkpala <francisakpala@yahoo.com>
2023-11-25 20:10:06 +0100

    Consuming Promises with async/await

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const fs = require("fs");
-const { resolve } = require("path");
 const superagent = require("superagent");
 
 const readFilePro = (file) => {
@@ -20,22 +19,41 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(res.body.message);
-    return writeFilePro("dog-img.txt", res.body.message);
 
-    // fs.writeFile("dog-img.txt", res.body.message, (err) => {
-    //   console.log("Random Dog image saved to file");
-    // });
-  })
-  .then(() => {
-    console.log("Random dog image saved to file");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro("dog-img.txt", res.body.message);
+    console.log("Random Dog image saved to file");
+  } catch (error) {
+    console.log(err);
+  }
+};
+
+getDogPic();
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Breed: ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(res.body.message);
+//     return writeFilePro("dog-img.txt", res.body.message);
+
+//     // fs.writeFile("dog-img.txt", res.body.message, (err) => {
+//     //   console.log("Random Dog image saved to file");
+//     // });
+//   })
+//   .then(() => {
+//     console.log("Random dog image saved to file");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
